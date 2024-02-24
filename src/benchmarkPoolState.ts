8598322322c71efcc1f10c788f60a18b2ff63e86
George Kurian Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2024-02-24 17:44:56 +0530

    Refactor benchmarkGetPoolState function and add error handling

--- src/benchmarkPoolState.ts
+++ src/benchmarkPoolState.ts
@@ -1,7 +1,18 @@
 import { ethers } from "ethers";
 
 export const benchmarkGetPoolState = async (provider: ethers.Provider) => {
-  const uniswapV3PoolAddress = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
+  const uniswapV3PoolAddresses = [
+    "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640",
+    "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8",
+    "0x4e68ccd3e89f51c3074ca5072bbac773960dfa36",
+    "0xcbcdf9626bc03e24f779434178a73a0b4bad62ed",
+    "0xc63b0708e2f7e69cb8a1df0e1389a98c35a76d52",
+    "0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+    "0x109830a1aaad605bbf02a9dfa7b0b92ec2fb7daa",
+    "0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8",
+    "0xe8c6c9227491c0a8156a0106a0204d881bb7e531",
+    "0x9db9e0e53058c89e5b94e29621a205198648425b",
+  ];
   const uniswapV3PoolAbi = [
     {
       inputs: [],
@@ -28,11 +39,13 @@ export const benchmarkGetPoolState = async (provider: ethers.Provider) => {
     },
   ];
 
-  const pool = new ethers.Contract(
-    uniswapV3PoolAddress,
-    uniswapV3PoolAbi,
-    provider
-  );
-  const slot0 = await pool.slot0();
-  // console.log(slot0);
+  for (const uniswapV3PoolAddress of uniswapV3PoolAddresses) {
+    const pool = new ethers.Contract(
+      uniswapV3PoolAddress,
+      uniswapV3PoolAbi,
+      provider
+    );
+    const slot0 = await pool.slot0();
+    // console.log(`Pool ${poolAddress}:`, slot0);
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -22,53 +22,67 @@ const main = async () => {
   const localProvider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
   const blockNumber = await freeProvider.getBlockNumber();
-  console.log("BlockNumber Free: ", blockNumber);
-  const blockNumberPaid = await paidProvider.getBlockNumber();
-  console.log("BlockNumber Paid: ", blockNumberPaid);
-  const blockNumberLocal = await localProvider.getBlockNumber();
-  console.log("BlockNumber Local: ", blockNumberLocal);
 
-  console.log("Starting BlockNumber benchmarks");
-
-  const startFreeBlockNumber = Date.now();
-  await benchmarkBlockNumber(freeProvider, blockNumber);
-  console.log(
-    `Free tier: ${(Date.now() - startFreeBlockNumber) / 1000} seconds`
-  );
-
-  const startPaidBlockNumber = Date.now();
-  await benchmarkBlockNumber(paidProvider, blockNumber);
-  console.log(
-    `Paid tier: ${(Date.now() - startPaidBlockNumber) / 1000} seconds`
-  );
-
-  const startLocalBlockNumber = Date.now();
-  await benchmarkBlockNumber(localProvider, blockNumber);
-  console.log(`Local: ${(Date.now() - startLocalBlockNumber) / 1000} seconds`);
-
-  console.log("Starting pool state benchmarks");
-
-  const startFreePoolState = Date.now();
-  for (let i = 0; i < 10; i++) {
-    await benchmarkGetPoolState(freeProvider);
+  console.log("Starting BlockNumber benchmarks\n\n");
+  try {
+    const startFreeBlockNumber = Date.now();
+    await benchmarkBlockNumber(freeProvider, blockNumber);
+    console.log(
+      `Free tier: ${(Date.now() - startFreeBlockNumber) / 1000} seconds`
+    );
+  } catch (error) {
+    console.log("Error in Free tier: ", error);
   }
-  console.log(
-    `Free tier: ${(Date.now() - startFreePoolState) / 10000} seconds`
-  );
-
-  const startPaidPoolState = Date.now();
-  for (let i = 0; i < 10; i++) {
-    await benchmarkGetPoolState(paidProvider);
+  try {
+    const startPaidBlockNumber = Date.now();
+    await benchmarkBlockNumber(paidProvider, blockNumber);
+    console.log(
+      `Paid tier: ${(Date.now() - startPaidBlockNumber) / 1000} seconds`
+    );
+  } catch (error) {
+    console.log("Error in Paid tier: ", error);
   }
-  console.log(
-    `Paid tier: ${(Date.now() - startPaidPoolState) / 10000} seconds`
-  );
-
-  const startLocalPoolState = Date.now();
-  for (let i = 0; i < 10; i++) {
-    await benchmarkGetPoolState(localProvider);
+  try {
+    const startLocalBlockNumber = Date.now();
+    await benchmarkBlockNumber(localProvider, blockNumber);
+    console.log(
+      `Local: ${(Date.now() - startLocalBlockNumber) / 1000} seconds`
+    );
+  } catch (error) {
+    console.log("Error in Local: ", error);
   }
-  console.log(`Local: ${(Date.now() - startLocalPoolState) / 10000} seconds`);
+  console.log("Starting pool state benchmarks\n\n");
+  try {
+    const startFreePoolState = Date.now();
+    for (let i = 0; i < 10; i++) {
+      await benchmarkGetPoolState(freeProvider);
+    }
+    console.log(
+      `Free tier: ${(Date.now() - startFreePoolState) / 10000} seconds`
+    );
+  } catch (error) {
+    console.log("Error in Free tier: ", error);
+  }
+  try {
+    const startPaidPoolState = Date.now();
+    for (let i = 0; i < 10; i++) {
+      await benchmarkGetPoolState(paidProvider);
+    }
+    console.log(
+      `Paid tier: ${(Date.now() - startPaidPoolState) / 10000} seconds`
+    );
+  } catch (error) {
+    console.log("Error in Paid tier: ", error);
+  }
+  try {
+    const startLocalPoolState = Date.now();
+    for (let i = 0; i < 10; i++) {
+      await benchmarkGetPoolState(localProvider);
+    }
+    console.log(`Local: ${(Date.now() - startLocalPoolState) / 10000} seconds`);
+  } catch (error) {
+    console.log("Error in Local: ", error);
+  }
 };
 
 main().catch((error) => {
