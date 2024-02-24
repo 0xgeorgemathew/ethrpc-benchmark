555227e2912846880f7e8d1e8f49c30d0ae2a2f0
George Kurian Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2024-02-25 00:01:35 +0530

    Fix formatting of average time output

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,7 @@ const main = async () => {
         (Date.now() - startLocalPoolState) / 1000
       } seconds`
     );
-    console.log(`Average time  :${averageTime.toFixed(4)} seconds`);
+    console.log(`Average time  : ${averageTime.toFixed(4)} seconds`);
   } catch (error) {
     console.log("Error in Local: ", error);
   }
